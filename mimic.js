8c7fb27c844e151dee493f049c1ce4c2a024d833
MimicRobot <mimicrobot@gmail.com>
2017-06-28 13:44:29 -0700

    c

--- mimic.js
+++ mimic.js
@@ -1,18 +1,12 @@
 /*
- *This program is free software: you can redistribute it and/or modify
- *it under the terms of the GNU General Public License as published by
- *the Free Software Foundation, either version 3 of the License, or
- *(at your option) any later version.
- *
- *This program is distributed in the hope that it will be useful,
- *but WITHOUT ANY WARRANTY; without even the implied warranty of
- *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *GNU General Public License for more details.
- *
- *You should have received a copy of the GNU General Public License
- *along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+  Mimic.js - adds custom scratch block to control the Mimic robot arm.
+  Created by Zaron Thompson, June 28, 2017.
+*/
+
 (function (ext) {
+
+	ext._errorMsg = "ERROR: scratch integration not enabled in Mimic software!";
+	ext._baseUrl = "http://localhost:8080/mimic/api/";
 
     ext._getStatus = function () {
         return { status: 2, msg: 'Ready' };
@@ -23,14 +17,14 @@
 
     ext.sayHello = function (text, callback) {
         $.ajax({
-            url: 'http://localhost:8080/hello/SayHello?name=' + text,
+            url: "_baseUrl" + "SayHello?name=" + text,
             dataType: 'jsonp',
             timeout : 2000,
             success: function (data) {
                 callback(data);
             },
             error: function (error) {
-                callback("ERROR: scratchTest program isn't running!");
+                callback(_errorMsg);
             }
         });
     };
@@ -42,6 +36,6 @@
         url: 'https://mimicrobot.github.io/scratch/'
     };
 
-    ScratchExtensions.register('ISS Tracker', descriptor, ext);
+    ScratchExtensions.register('Mimic robot arm', descriptor, ext);
 
 })({});
