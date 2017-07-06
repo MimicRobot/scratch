89fd8eb3ee4f1cccf7f48b26d2d9129d8e5c525a
MimicRobot <mimicrobot@gmail.com>
2017-07-06 13:39:35 -0700

    c

--- mimic.js
+++ mimic.js
@@ -237,7 +237,7 @@
 			listenForEvents();
 		}, function(){
 			//failed
-			ScratchExtensions.register('Mimic robot arm', {blocks: [['r', 'failed to connect to your robot arm', 'failedConnection']]}, ext);
+			ScratchExtensions.register('Mimic robot arm', {blocks: [['r', 'failed to connect - refresh', 'failedConnection']]}, ext);
 		});
 	};
 	register();
